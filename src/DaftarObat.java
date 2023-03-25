e2288a3f4bd6201c27efb0061b5bac9a1de58547
ASUS <dewi85280@gmail.com>
2023-03-25 16:31:29 +0700

    TM_01

---
+++ src/DaftarObat.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class DaftarObat {
+    private JLabel judul;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton keluarButton;
+    private JButton submitButton;
+}
